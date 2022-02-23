8b4fa20cebaa63f1e17e169d06fe5fc8d97d7825
Liz <lizgreen987@gmail.com>
2022-02-23 16:44:35 +0000

    Update about-tss.svelte

--- src/routes/about-tss.svelte
+++ src/routes/about-tss.svelte
@@ -1,8 +1,20 @@
 <h1>About The small Society</h1>
-<h2>About Us</h2>
-
-<div class="flex justify-around bg-violet-500">
- 
+<br>
+<div>
+ <p>The small Society is being established to promote the mental health and wellness of individuals and to support the 
+    recovery of persons experiencing mental illness, particularly where individuals are unwilling or unable to access
+    support from the NHS or existing social services. 
+ </p>
+<br>
+ <p>It's primary aim is to support Carers of the 'cared for' with particular reference to those caring from mental
+     disorders, in particular but not exclusively by providing a place of refuge for a short term stay and also by 
+     providing funds for respite purposes.
+ </p>
+<br>
+ <p>The small Society also aims to provide or produce information and resources that will further the understanding
+     of issues around mental health with a particular reference to the needs of carers and those supporting individuals
+     with a mental health condition.
+ </p>
 </div>
 
 
