0d35998d18d13c4cffe94eb104dbec28ca7b5786
lucyhollyer <lucyhollyer@hotmail.co.uk>
2022-02-25 13:34:16 +0000

    squash

--- src/routes/about-tss.svelte
+++ src/routes/about-tss.svelte
@@ -1,20 +1,24 @@
-<h1>About The small Society</h1>
-<br>
-<div>
- <p>The small Society is being established to promote the mental health and wellness of individuals and to support the 
-    recovery of persons experiencing mental illness, particularly where individuals are unwilling or unable to access
-    support from the NHS or existing social services. 
- </p>
-<br>
- <p>Its primary aim is to support Carers of the 'cared for' with particular reference to those caring from mental
-     disorders, in particular but not exclusively by providing a place of refuge for a short term stay and also by 
-     providing funds for respite purposes.
- </p>
-<br>
- <p>The small Society also aims to provide or produce information and resources that will further the understanding
-     of issues around mental health with a particular reference to the needs of carers and those supporting individuals
-     with a mental health condition.
- </p>
-</div>
+<script>
+	import Container from '$lib/components/container.svelte';
+</script>
 
-
+<Container>
+    <h1>About The small Society</h1>
+    <br>
+    <div>
+    <p>The small Society is being established to promote the mental health and wellness of individuals and to support the
+        recovery of persons experiencing mental illness, particularly where individuals are unwilling or unable to access
+        support from the NHS or existing social services.
+    </p>
+    <br>
+    <p>Its primary aim is to support Carers of the 'cared for' with particular reference to those caring from mental
+        disorders, in particular but not exclusively by providing a place of refuge for a short term stay and also by
+        providing funds for respite purposes.
+    </p>
+    <br>
+    <p>The small Society also aims to provide or produce information and resources that will further the understanding
+        of issues around mental health with a particular reference to the needs of carers and those supporting individuals
+        with a mental health condition.
+    </p>
+    </div>
+</Container>
