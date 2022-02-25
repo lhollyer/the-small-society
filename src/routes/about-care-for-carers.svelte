2c68adff9ea917b11a5972c501a87a5296ba0c3c
Liz <lizgreen987@gmail.com>
2022-02-25 15:10:34 +0000

    Adding container

--- src/routes/about-care-for-carers.svelte
+++ src/routes/about-care-for-carers.svelte
@@ -1,5 +1,11 @@
-<h1>About Care for Carers</h1>
-<br>
-<div>
- <p>Insert text here</p>
-</div>
+<script>
+	import Container from '$lib/components/container.svelte';
+</script>
+
+<Container>
+    <h1>About Care for Carers</h1>
+    <br>
+    <div>
+    <p>Insert text here</p>
+    </div>
+</Container>
