ce95208bc6a8db9d655ac1aa2ead38e12c1af7e5
Abbie Cornelius <abbyc@sky.com>
2022-04-08 17:04:33 +0100

    initial contact-us

---
+++ src/routes/contact-us.svelte
@@ -0,0 +1,56 @@
+<script>
+	import Container from '$lib/components/container.svelte';
+</script>
+
+<Container>
+    <h1>Contact Us!</h1>
+    <div>
+        <p>
+            If you have any questions, please feel free to get in touch.
+        </p>
+        <br>
+        <p>
+            Please allow for a turnaround for us to read your message and reply to you - we hope this to take no longer than a week. 
+        </p>
+        <form>
+            <div>
+                <label for="name">Email</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  value=""
+                />
+            </div>
+            <div>
+              <label for="name">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value=""
+              />
+          </div>
+            <button type="submit">Submit</button>
+          </form>
+</Container>
+
+<style lang="postcss">
+    * {
+        box-sizing: border-box;
+        }
+        form {
+        display: flex;
+        flex-direction: column;
+        width: 300px;
+        }
+
+        form > div{
+        display: flex;
+        justify-content: space-between;
+        }
+
+        form > div + * {
+        margin-top: 10px;
+        }
+</style>
