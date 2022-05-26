62c53f5cd278d8064a78ce1eda9791b6457a8d02
Abbie Cornelius <abbyc@sky.com>
2022-05-26 17:18:21 +0100

    change of contact us form to embedded google form

--- src/routes/contact-us.svelte
+++ src/routes/contact-us.svelte
@@ -1,50 +1,23 @@
 <script>
 	import Container from '$lib/components/container.svelte';
-
-  const submitForm = async (data) => {
-    const formData = new FormData(data.currentTarget);
-
-    const res = await fetch("contact.json", {
-      method: "POST",
-      body: formData,
-    });
-
-  };
 </script>
 
 <Container>
     <h1>Contact Us!</h1>
-    <div>
         <p>
-            If you have any questions, please feel free to get in touch.
-        </p>
+            If you have any questions, please use the below form to get in touch. Alternatively you can email us at <a href="mailto:thesmallsocietycharity@gmail.com"> thesmallsocietycharity@gmail.com </a></p>
         <br>
         <p>
-            Please allow for a turnaround for us to read your message and reply to you - we hope this to take no longer than a week. 
+            Please allow for a turnaround for us to read your message and reply to you. 
         </p>
 
-
-        <form on:submit|preventDefault="{submitForm}">
-          <div>
-            <label for="">
-              Email
-            </label>
-            <input type="email" id="email" name="email" value="" />
-          </div>
-          <div>
-            <label for="">
-              Your Query
-            </label>
-            <input type="text" id="query" name="query" value="" />
-          </div>
-          <div>
-            <input type="submit" /> 
-          </div>
-        </form> 
+        <br>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScIT0GHeP1OOep2p5I1-fcd5_bxE_0nErYCOH-OmivOQ-MaEg/viewform?embedded=true" width="730" height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </Container>
 
 <style lang="postcss">
-  div {
-    margin-bottom: 1em;
-  }
+  	a {
+		@apply text-header-blue font-semibold
+	}
 </style>
