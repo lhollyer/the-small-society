47a636d19ab72fce9851573132f6c96e6f00f4aa
lucyhollyer <lucyhollyer@hotmail.co.uk>
2022-07-08 10:18:20 +0100

    attempt at getting mobile navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -8,12 +8,71 @@
 	</div>
 	<a href="/">Support Us</a>
 </div>
+-->
 
 <style lang="postcss">
-	div {
+	/* div {
 		@apply bg-light-blue text-slate-800 h-16 font-bold flex items-center whitespace-nowrap;
-	}
+	} */
 	a {
-		@apply px-5 text-xl flex hover:bg-navbar-hover hover:transition-all delay-75 h-full items-center justify-center;
+		@apply px-5 text-xl flex hover:bg-navbar-hover hover:transition-all delay-75 h-full items-center;
 	}
 </style>
+
+
+<nav class="
+  relative
+  w-full
+  flex flex-wrap
+  items-center
+  justify-between
+  py-4
+  bg-light-blue
+  text-slate-800
+  shadow-lg
+  navbar navbar-expand-lg navbar-light
+  ">
+  <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+		<button class="
+				navbar-toggler
+				border-0
+				hover:shadow-none hover:no-underline
+				py-2
+				px-2.5
+			" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+			<!-- create hamburger icon -->
+			<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
+			class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+				<path fill="currentColor"
+					d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+				</path>
+			</svg>
+		</button>
+
+		<div class="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
+			<a class="
+						flex
+						items-center
+						mt-2
+						lg:mt-0
+						mr-1
+					" href="/">
+				The small Society
+			</a>
+
+			<!-- middle links -->
+			<ul class="navbar-nav flex flex-col list-style-none lg:justify-center">
+				<li class="nav-item p-3 w-full">
+					<a class="nav-link" href="/about-care-for-carers">About Care for Carers</a>
+				</li>
+				<li class="nav-item p-3 w-full">
+					<a class="nav-link" href="/gallery">Gallery</a>
+				</li>
+				<li class="nav-item p-3 w-full">
+					<a class="nav-link" href="/contact-us">Contact Us</a>
+				</li>
+			</ul>
+
+		</div>
+  </div>
+</nav>
