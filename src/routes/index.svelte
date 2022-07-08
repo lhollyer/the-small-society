bef4cb8b396aa80218946806c038038dcab4de02
Abbie Cornelius <abbyc@sky.com>
2022-07-08 15:03:34 +0100

    suggested update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,7 @@
 	<div class="w-full">
 		<h1>Welcome to The small Society</h1>
 		<p>
-			Pete Dennis is the founder of the SMALL society, the umbrella organisation for
+			Pete Dennis is the founder of The small Society, the umbrella organisation for
 			Care for Carers.
 		</p>
 		<p>
@@ -24,62 +24,42 @@
 			House), but that he had got side-tracked and accidentally added two more separate, living
 			spaces - which meant he had room to spare.
 		</p>
-	</div>
 
-	<div class="pt-10 pb-1">
-		<p> Find out more about The small Society and what its goals are. </p>
-	</div>
-	<div class="flex justify-around h-96">
-		<a href="/gallery" class="relative h-96">
-			<div class="absolute w-full h-1/3 bg-gradient-to-l from-gray-50 to-transparent right-0 mt-0.5 mr-0.5 opacity-80">
-				<h3 class="absolute right-0">About The small Society</h3>
+		<a href="/about-tss">
+			<div class="mt-10 p-2 bg-light-blue border-4 border-header-blue hover:border-slate-800">	
+				<h2> Want to know more about us?</h2>
+				<p>
+					Want to find out more about The small Society and what we provide? Please take a look here for more information.
+				</p>
 			</div>
-			<img
-				class="object-scale-down h-96 w-120 border-2 border-header-blue"
-				src="house.jpeg"
-				alt="Front view of the house"
-			/>
 		</a>
-	</div>
 
-	<div class="pt-10 pb-1">
-		<p> Find out more about Care for Careers and what its goals are. </p>
-	</div>
-	<div class="flex justify-around h-96">
-		<a href="/gallery" class="relative h-96">
-			<div class="absolute w-full h-1/3 bg-gradient-to-l from-gray-50 to-transparent right-0 mt-0.5 mr-0.5 opacity-80">
-				<h3 class="absolute right-0">About Care for Careers</h3>
+		<a href="/gallery">
+			<div class="mt-10 p-2 bg-light-blue border-4 border-header-blue hover:border-slate-800">	
+				<h2>Have a look around! </h2>
+				<p>
+					We have numerous photos from both our accomodation and the local area for you to look through. 
+				</p>
 			</div>
-			<img
-				class="object-scale-down h-96 w-120 border-2 border-header-blue"
-				src="rustic.jpeg"
-				alt="Rustic accommodation view"
-			/>
 		</a>
-	</div>
 
-	<div class="pt-10 pb-1">
-		<p> Take a look through our Gallery and see more of our site & the local area. </p>
-	</div>
-	<div class="flex justify-around h-96">
-		<a href="/gallery" class="relative h-96">
-			<div
-				class="absolute w-full h-1/3 bg-gradient-to-l from-gray-50 to-transparent right-0 mt-0.5 mr-0.5 opacity-80"
-			>
-				<h3 class="absolute right-0">Gallery</h3>
+		<a href="/contact-us">
+			<div class="mt-10 p-2 bg-light-blue border-4 border-header-blue hover:border-slate-800">	
+				<h2>Get in touch </h2>
+				<p>
+					If you have any questions, please feel free to either email us or use the provided form with any questions you many have.
+				</p>
 			</div>
-			<img
-				class="object-scale-down h-96 w-120 border-2 border-header-blue"
-				src="deal.jpeg"
-				alt="Deal beach"
-			/>
 		</a>
-	</div>
 
+	</div>
 </Container>
 
 <style lang="postcss">
 	p {
 		@apply pb-5;
+	}
+	h2 {
+		@apply text-center;
 	}
 </style>
