9b64fc8d78a06fdaf44680c48219a8e7b8c8ab30
Kyle Barth <kylebarth@me.com>
2022-07-08 17:57:26 +0100

    navbar styling fixed

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -31,32 +31,53 @@
   text-slate-800
   shadow-lg
   navbar navbar-expand-lg navbar-light
-  ">
-  <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-		<button class="
+  "
+>
+	<div class="container-fluid w-full flex flex-wrap items-center sm:justify-center px-6">
+		<button
+			class="
 				navbar-toggler
 				border-0
 				hover:shadow-none hover:no-underline
 				py-2
 				px-2.5
-			" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+			"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarSupportedContent"
+		>
 			<!-- create hamburger icon -->
-			<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
-			class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-				<path fill="currentColor"
-					d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
-				</path>
+			<svg
+				aria-hidden="true"
+				focusable="false"
+				data-prefix="fas"
+				data-icon="bars"
+				class="w-6"
+				role="img"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 448 512"
+			>
+				<path
+					fill="currentColor"
+					d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+				/>
 			</svg>
 		</button>
 
-		<div class="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
-			<a class="
+		<div
+			class="collapse navbar-collapse flex-grow items-center justify-between"
+			id="navbarSupportedContent"
+		>
+			<a
+				class="
 						flex
 						items-center
 						mt-2
 						lg:mt-0
 						mr-1
-					" href="/">
+					"
+				href="/"
+			>
 				The small Society
 			</a>
 
@@ -73,6 +94,27 @@
 				</li>
 			</ul>
 
+			<div class="w-[213px]" />
 		</div>
-  </div>
+	</div>
 </nav>
+
+<!-- <div class="w-full justify-between ">
+	<a href="/">The small Society</a>
+	<div>
+		<a href="/about-tss">About TsS</a>
+		<a href="/">About Care for Carers</a>
+		<a href="/gallery">Gallery</a>
+		<a href="/contact-us">Contact Us</a>
+	</div>
+	<a href="/">Support Us</a>
+</div>
+-->
+<style lang="postcss">
+	/* div {
+		@apply bg-light-blue text-slate-800 h-16 font-bold flex items-center whitespace-nowrap;
+	} */
+	a {
+		@apply px-5 text-xl flex hover:bg-navbar-hover hover:transition-all delay-75 h-full items-center;
+	}
+</style>
