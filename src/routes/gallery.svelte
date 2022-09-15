0f1b4ab5814968854037c7b6d9f098aaaffe229a
lucyhollyer <lucyhollyer@hotmail.co.uk>
2022-09-15 16:35:17 +0100

    Add garden carousel, rename photos & improve carousel code

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -1,5 +1,6 @@
 <script>
 	import HouseCarousel from '$lib/components/Carousel/houseCarousel.svelte';
+	import GardenCarousel from '$lib/components/Carousel/gardenCarousel.svelte';
 	import LocalAreaCarousel from '$lib/components/Carousel/localAreaCarousel.svelte';
 	import Container from '$lib/components/container.svelte';
 </script>
@@ -7,11 +8,12 @@
 <Container>
 	<h1>Gallery</h1>
 	<h2>Pictures of the accommodation</h2>
-
 	<HouseCarousel />
 
-	<h2>Pictures of the area</h2>
+	<h2>Pictures of the garden</h2>
+	<GardenCarousel />
 
+	<h2>Pictures of the area</h2>
 	<LocalAreaCarousel />
 
 </Container>
